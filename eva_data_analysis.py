441b1b7d2e88cc74a948f62ab5ec992000533ce0
Alejandra Torres <ma.torresmeraz@gmail.com>
2025-08-25 11:45:07 -0500

    eva image name update'

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,7 +1,7 @@
 # https://data.nasa.gov/resource/eva.json (with modifications)
-data_f = open('/Users/alejandra/Documents/Carpentries Workshops/spacewalks/eva-data.json', 'r')
-data_t = open('/Users/alejandra/Documents/Carpentries Workshops/spacewalks/eva_data_analysis.py','w')
-g_file = 'cumulative_eva_graph.png'
+data_f = open('./eva-data.json', 'r')
+data_t = open('./eva-data.csv','w')
+g_file = './cumulative_eva_graph.png'
 
 fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
 
