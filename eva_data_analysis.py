777616e79443b1c2d9ad678f5c8ae75872adae1e
Alejandra Torres <ma.torresmeraz@gmail.com>
2025-08-26 11:51:29 -0500

    Added comments on code

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -2,22 +2,28 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Data source: https://data.nasa.gov/resource/eva.json (with modifications)
+
+# Defining variable names
 input_file = open('./eva-data.json', 'r', encoding='ascii')
 output_file = open('./eva-data.csv', 'w', encoding='utf-8')
-graph_file = './cumulative_eva_graph.png'
+graph_file = './cumulative_eva_graph.png' 
 
-eva_df = pd.read_json(input_file, convert_dates=['date'])
-eva_df['eva'] = eva_df['eva'].astype(float)
-eva_df.dropna(axis=0, inplace=True)
-eva_df.sort_values('date', inplace=True)
+# Read in the data from JSON file into pandas df
+eva_df = pd.read_json(input_file, convert_dates=['date']) 
+eva_df['eva'] = eva_df['eva'].astype(float) 
+eva_df.dropna(axis=0, inplace=True) 
+eva_df.sort_values('date', inplace=True) 
+eva_df.to_csv(output_file, index=False) 
 
-eva_df.to_csv(output_file, index=False)
+# Convert eva duration data to hours and calculates cumulative hours
+eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60) 
+eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum() 
 
-eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
-eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
-plt.plot(eva_df['date'], eva_df['cumulative_time'], 'ko-')
+# Plot cumulative hours over time
+plt.plot(eva_df['date'], eva_df['cumulative_time'], 'ko-') 
 plt.xlabel('Year')
 plt.ylabel('Total time spent in space to date (hours)')
 plt.tight_layout()
 plt.savefig(graph_file)
 plt.show()
+
